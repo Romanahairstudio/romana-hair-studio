fb0fed351280d1f8e10fc3e54318456b6ddf5a1a
souzafabio-romana <souza.fabio@gmail.com>
2026-06-15 21:56:07 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -28,7 +28,7 @@ export default function RomanaHairLandingPage() {
   const showcase = [
     '/images/Cintia2-1.webp',
     '/images/depois-tati.webp',
-    '/images/depois-dirce.webp',
+    '/images/h2.webp',
     '/images/depois-paloma.webp'
   ]
 
